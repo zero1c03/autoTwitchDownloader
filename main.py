0cbb3bc4c22afb14294e81b1b1a4311c09fc8e71
Shang Wei <zero1c03@hotmail.com>
2018-03-12 23:42:39 +0800

    add auto switch linux, windows mode. add auto switch python 3, python 2. add check streamer name and folder path.

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import argparse
 # for python 3.x
-import urllib.request
-
+try:
+    import urllib.request
+except:
+    print("import urllib2")
 # for python 2.7
-# import urllib2
+try:
+    import urllib2
+except:
+    print ("import urllib.request")
+
 import json
 import time
 import os
 import subprocess
 import datetime
 import sys
+import codecs
+from sys import platform
 
 clientID = "xjjsgz3mrmduq7241fuuk724nnmgcq"
 checkTime = 5
 
+version = 0
 dot = 0
 printDot = "."
 
@@ -34,16 +43,18 @@ def check_user(user):
     data = None
     url = "https://api.twitch.tv/kraken/streams/" + user
 
-    # for python 3.x
-    request = urllib.request.Request(url, headers={"Client-ID": clientID})
-    opener = urllib.request.build_opener()
-    contents = opener.open(request)
-    data = json.load(contents)
-
-    # for python 2.7
-    # request = urllib2.Request(url, headers={"Client-ID": clientID})
-    # contents = urllib2.urlopen(request).read()
-    # data = json.loads(contents)
+    if version == 3:
+        # for python 3.x
+        request = urllib.request.Request(url, headers={"Client-ID": clientID})
+        opener = urllib.request.build_opener()
+        contents = opener.open(request)
+        reader = codecs.getreader("utf-8")
+        data = json.load(reader(contents))
+    else:
+        # for python 2.7
+        request = urllib2.Request(url, headers={"Client-ID": clientID})
+        contents = urllib2.urlopen(request).read()
+        data = json.loads(contents)
     
     if data["stream"] == None:
         status = 0
@@ -83,36 +94,43 @@ def check_loop(user):
             printDot = "."
 
         if status == 0:
-            # for python 3.x
-            print(user, "offline, try after", checkTime,
-                  "seconds"+printDot, end="\r")
-
-            # for python 2.7
-            # print(user, "offline, try after", checkTime,
-            #       "seconds"+printDot+"      \r")
+            if version == 3:
+                # for python 3.x
+                print(user, "offline, try after", checkTime,
+                    "seconds"+printDot, end="\r")
+            else:
+                # for python 2.7
+                print(user, "offline, try after", checkTime,
+                      "seconds"+printDot+"      \r")
             time.sleep(checkTime)
         elif status == 1:
-            # for python 3.x
-            print(user, "streaming, start download!" + printDot, end="\n")
+            if version == 3:
+                # for python 3.x
+                print(user, "streaming, start download!" + printDot, end="\n")
 
-            # for python 2.7
-            # print(user, "streaming, start download!" + printDot + "     \r")
+            else:
+                # for python 2.7
+                print(user, "streaming, start download!" + printDot + "     \r")
 
             filename = user + "-" + datetime.datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss") + ".mp4"
 
             # print(filename)
             streamURL = "twitch.tv/" + user
 
+            if  platform == "win32":
             # Windows version.
-            dir_path = os.path.dirname(os.path.realpath(__file__))
-            filename = location + "\\" + filename
-            subprocess.call([dir_path+"\\Streamlink_for_Windows_Portable_v0.10.0"+'\\Streamlink.exe',
-                             streamURL, 'best', '-o',  filename])
+                print("windows mode")
+                dir_path = os.path.dirname(os.path.realpath(__file__))
+                filename = location + "\\" + filename
+                subprocess.call([dir_path+"\\Streamlink_for_Windows_Portable_v0.10.0"+'\\Streamlink.exe',
+                                streamURL, 'best', '-o',  filename])
 
+            elif platform == "linux" or platform == "linux2":
             # Linux version
-            # filename = location + "/" + filename
-            # subprocess.call(["streamlink",
-            #                  streamURL, 'best', '-o',  filename])
+                print("linux mode")
+                filename = location + "/" + filename
+                subprocess.call(["streamlink",
+                                 streamURL, 'best', '-o',  filename])
 
             print("Download finished, start checking again. \n")
             time.sleep(checkTime)
@@ -120,7 +138,22 @@ def check_loop(user):
             print("The filepath or streamer name error, Please check. \n")
             running = False
 
-print(sys.version)
+print("Python version = ", sys.version_info[0])
+
+if sys.version_info[0] == 3:
+    version = 3
+else:
+    version = 2
+    
 user = parse_args().u
 location = parse_args().f
-check_loop(user)
+
+# user = "wannasinging"
+# location = "F:\Stream"
+
+if user == None:
+    print("please '-u User' to input streamer")
+elif location == None:
+    print("please '-f Folder' to input downaload folder")
+else:
+    check_loop(user)
